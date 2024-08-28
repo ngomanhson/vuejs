90fe0f054ba3c935cf4faa8807a3e777d5ab367b
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-08-28 22:14:35 +0700

    Dynamic classes

--- basic-2/main.js
+++ basic-2/main.js
@@ -7,21 +7,29 @@ const app = Vue.createApp({
                     thumbnail: "./assets/images/image-1.jpg",
                     name: "Product 1",
                     price: 120.0,
+                    isCart: false,
                 },
                 {
                     thumbnail: "./assets/images/image-2.jpg",
                     name: "Product 2",
                     price: 120.0,
+                    isCart: false,
                 },
                 {
                     thumbnail: "./assets/images/image-3.jpg",
                     name: "Product 3",
                     price: 120.0,
+                    isCart: true,
                 },
             ],
         };
     },
-    methods: {},
+    methods: {
+        onToggleCart(event, product) {
+            console.log(product);
+            product.isCart = !product.isCart;
+        },
+    },
 });
 
 app.mount("#contact");
