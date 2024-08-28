3af74be6ad0d70e6b1cab8e3581511a36f2edc1b
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-08-28 21:53:25 +0700

    Attribute binding

--- basic-2/main.js
+++ basic-2/main.js
@@ -1,16 +1,20 @@
 const app = Vue.createApp({
     data() {
         return {
+            classTesting: "flex",
             products: [
                 {
+                    thumbnail: "./assets/images/image-1.jpg",
                     name: "Product 1",
                     price: 120.0,
                 },
                 {
+                    thumbnail: "./assets/images/image-2.jpg",
                     name: "Product 2",
                     price: 120.0,
                 },
                 {
+                    thumbnail: "./assets/images/image-3.jpg",
                     name: "Product 3",
                     price: 120.0,
                 },
